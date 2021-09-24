f16d64f38774b4aa556b50c098b7d1ecb52ae7c6
gayatrivjoshi <gayatrij768@gmail.com>
2021-09-24 08:58:37 +0530

    Setup react, tailwind and react router

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors:{
+        primary: "#FAFAFA",
+        secondary:{
+          lightgray: "#3A4750",
+          darkgray:"#303841",
+          red:"#F24D5D",
+          lightred:"#FF6E7C",
+          brightred:"#F84052",
+          blue:"#5778FF"
+        }
+      },
+      screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    fontFamily: {
+      serif: 'Ibarra Real Nova',
+      sans: 'Montserrat'
+    },
+    boxShadow: {
+      '2xl': '0 -20px 70px -12px rgba(0, 0, 0, 0.15)',
+      'xl': '0 0px 50px -5px rgba(0,0,0,0.4)',
+      "3xl": '0 25px 50px 0px rgba(0, 0, 0, 0.15)',
+      'md': '0 8px 25px -7px rgba(0, 0, 0, 0.4)',
+      inner: 'inset 0 0 20px 2px rgba(0, 0, 0, 0.3)',
+      'pn': '0 10px 15px 5px rgba(0, 0, 0, 0.2)',
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
